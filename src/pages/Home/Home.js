7b21a629e701956fe89d1033393b50660053e39f
Gordon Stevens <gsteven5@my.centennialcollege.ca>
2019-07-10 04:42:20 -0400

    Add files via upload
    Completely debugged, changelog: web301-assignment-2-debug-log-gordonstevens.txt

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Card from '../../components/Card/Card';
+
+const Home = () => {
+   return (
+      <div>
+         <Card>
+            <h2>You fixed the Home Page!</h2>
+            <p>Here is a nice hint</p>
+            <p>Whenever you run into errors that start with "Unexpected token...", it probably means your previous line of code is incomplete.</p>
+         </Card>
+      </div>
+   );
+};
+
+export default Home;
